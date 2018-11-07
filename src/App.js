a9e20a2412eecd75f2d7a4b4e4c414f3fa5c0491
shambho <Sivakrishna.Karasala@eku.edu>
2018-11-07 18:20:48 -0500

    finish content with dynamic control buttons

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import './App.css';
 
 const Logo = styled.div`
@@ -7,31 +7,54 @@ const Logo = styled.div`
 `;
 
 const ControlButton = styled.div`
+  cursor: pointer;
+  ${props => props.active && css`
+  text-shadow: 0px 0px 60px #03ff03` }
+`;
+
+const Bar = styled.div`
+  display: grid;
+  grid-template-columns: 222px auto 100px 100px;
+  margin-bottom: 40px;
 
 `;
 
 const AppLayout = styled.div`
   padding: 40px;
-  display: grid;
-  grid-template-columns: 222px auto 100px 100px;
+`;
+
+const Content = styled.div`
 
 `;
+
 class App extends Component {
+  state = {
+    page: 'dashboard'
+  }
+  displayingDashboard = () => this.state.page === 'dashboard'
+  displayingSettings = () => this.state.page === 'settings'
   render() {
     return (
       <AppLayout>
+      <Bar>
       <Logo>
         Aum Namah Shivaya
       </Logo>
       <div>
 
       </div>
-      <ControlButton>
+      <ControlButton onClick={() => {this.setState({page:'dashboard'})}} 
+      active={this.displayingDashboard()}>
         Dashboard
       </ControlButton>
-      <ControlButton>
+      <ControlButton onClick={() => {this.setState({page:'settings'})}} 
+      active={this.displayingSettings()}>
         Settings
       </ControlButton>
+      </Bar>
+      <Content>
+        hara hara mahadeva shambho shankara {this.state.page}
+      </Content>
       </AppLayout>
     );
   }
