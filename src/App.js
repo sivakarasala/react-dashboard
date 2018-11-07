39f4e6bdbb3e91723942b5ba772a52f94eef3ad0
shambho <Sivakrishna.Karasala@eku.edu>
2018-11-07 17:47:59 -0500

    added styled components, remove boilerplate styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
 
+const CustomElement = styled.div`
+  color: skyblue;
+  font-size: 30px;
+`;
 
+const BlueElement = styled(CustomElement)`
+  color: orange;
+`;
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div>
+      <CustomElement>
         Aum Namah Shivaya
+      </CustomElement>
+      <BlueElement>
+        Shambho Shankara
+      </BlueElement>
       </div>
     );
   }
