f831a5494db572196ee95ace381f6ae2f39d04a2
shambho <Sivakrishna.Karasala@eku.edu>
2018-11-08 11:17:14 -0500

    Create coingrid

---
+++ src/Shared/Tile.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+import {subtleBoxShadow,
+lightBlueBackground, greenBoxShadow,
+redBoxShadow } from './Styles';
+
+export const Tile = styled.div`
+    ${subtleBoxShadow}
+    ${lightBlueBackground}
+    padding: 10px;
+    text-align: center;
+`;
+
+export const SelectableTile = styled(Tile)`
+    &:hover {
+        cursor: pointer;
+        ${greenBoxShadow}
+    }
+`;
