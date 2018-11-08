d9f475ef48f0d7a43831bac8bf950af915abb13d
shambho <Sivakrishna.Karasala@eku.edu>
2018-11-08 09:09:25 -0500

    refactor: add appbar

--- src/App/AppBar.js
+++ src/App/AppBar.js
@@ -1,38 +1,15 @@
 import styled, { css } from "styled-components";
 import React, { Component } from 'react';
 
-const Logo = styled.div`
-  font-size: 1.5em;
-`;
-
-const ControlButton = styled.div`
-  cursor: pointer;
-  ${props => props.active && css`
-  text-shadow: 0px 0px 60px #03ff03` }
-`;
-
 const Bar = styled.div`
   display: grid;
-  grid-template-columns: 222px auto 100px 100px;
-  margin-bottom: 40px;
-
+  grid-template-columns: 180px auto 100px 100px;
 `;
-
-export default function() {
-    return  <Bar>
-    <Logo>
-      Aum Namah Shivaya
-    </Logo>
-    <div>
-
-    </div>
-    {!this.state.firstVisit && (<ControlButton onClick={() => {this.setState({page:'dashboard'})}} 
-    active={this.displayingDashboard()}>
-      Dashboard
-    </ControlButton>)}
-    <ControlButton onClick={() => {this.setState({page:'settings'})}} 
-    active={this.displayingSettings()}>
-      Settings
-    </ControlButton>
+export default function(){
+  return <Bar>
+    <div>CryptoDash</div>
+    <div></div>
+    <div>Dashboard</div>
+    <div>Settings</div>
     </Bar>
 }
--- src/App/index.js
+++ src/App/index.js
@@ -1,78 +1,20 @@
 import React, { Component } from 'react';
 import styled, { css } from 'styled-components';
 import './App.css';
+import AppLayout from './AppLayout';
+import WelcomeMessage from './WelcomeMessage';
 import AppBar from './AppBar';
-const cc = require('cryptocompare');
 
 
-const AppLayout = styled.div`
-  padding: 40px;
-`;
-
-const Content = styled.div`
-
-`;
-
-const checkFirstVisit = () => {
-  let cryptoDashData = localStorage.getItem('cryptoDash');
-  if(!cryptoDashData) {
-    return {
-      firstVisit: true,
-      page: 'settings'
-    }
-  }
-  return {};
-}
 
 class App extends Component {
-  state = {
-    page: 'settings',
-    ...checkFirstVisit()
-  }
-  componentDidMount = () => {
-    this.fetchCoins();
-  }
-  fetchCoins = async () => {
-    let coinList = (await cc.coinList()).Data;
-    this.setState({ coinList });
-
-  }
-  displayingDashboard = () => this.state.page === 'dashboard';
-  displayingSettings = () => this.state.page === 'settings';
-  firstVisitMessage = () => {
-    if(this.state.firstVisit){
-      return <div>Welcome to CyptoDash, please select your favourite coins to begin.</div>
-    }
-  }
-  confirmFavourites = () => {
-    localStorage.setItem('cryptoDash', 'shiva');
-    this.setState({
-      firstVisit: false,
-      page: 'dashboard'
-    })
-  }
-  settingsContent = () => {
-    return <div>
-      {this.firstVisitMessage()}
-      <div onClick={this.confirmFavourites}>
-        Confirm Favourites
-      </div>
-    </div>
-  }
-  loadingContent = () => {
-    if(!this.state.coinList) {
-      return <div> Loading Coins </div>
-    }
-  }
   render() {
     return (
       <AppLayout>
-        {AppBar.call(this)}
-      {this.loadingContent() || <Content>
-        {this.displayingSettings() && this.settingsContent()}
-      </Content>}
+        <AppBar/>
+        <WelcomeMessage/>
       </AppLayout>
-    );
+    )
   }
 }
 
