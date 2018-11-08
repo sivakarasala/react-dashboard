e0142534e87c4777753d9f83fe6d4e956a2fa2e0
shambho <Sivakrishna.Karasala@eku.edu>
2018-11-08 10:41:46 -0500

    Add CryptoCompare

--- src/App/AppProvider.js
+++ src/App/AppProvider.js
@@ -1,4 +1,5 @@
 import React from 'react';
+const cc = require('cryptocompare');
 
 export const AppContext = React.createContext();
 
@@ -12,6 +13,14 @@ export class AppProvider extends React.Component {
             confirmFavorites: this.confirmFavorites
         }
     }
+    componentDidMount = () => {
+        this.fetchCoins();
+    };
+    fetchCoins = async() => {
+        let coinList = (await cc.coinList()).Data;
+        this.setState({coinList});
+    }
+
     confirmFavorites= () => {
         this.setState({
             firstVisit: false,
